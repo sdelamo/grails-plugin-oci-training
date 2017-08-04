d6f41e73127ff0751defe356fec64a2e8605eee4
sdelamo <sergio.delamo@softamo.com>
2017-08-04 06:42:19 +0200

    0.0.5

--- travis-build.sh
+++ travis-build.sh
@@ -22,6 +22,7 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST
   if [[ -n $TRAVIS_TAG ]]; then
       echo "Pushing build to Bintray"
       ./gradlew :oci-training:bintrayUpload || EXIT_STATUS=$?
+      ./gradlew :oci-training-scraper:bintrayUpload || EXIT_STATUS=$?
   else
       echo "Publishing snapshot to OJO"
       ./gradlew :oci-training:artifactoryPublish || EXIT_STATUS=$?
